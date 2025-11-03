9ba7701906bcc2f692165eccea08f346d82f0fd1
Yuvi <yuvraj21981@gmail.com>
2025-11-03 11:52:10 -0800

    Add post class with __inti__ , __eq__, __str__

---
+++ Blogging/post.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+class Post:
+    """
+    Represents a blog post with code, title, text, and timestamps.
+    """
+
+    def __init__(self, code: int, title: str, text: str):
+        self.code = code
+        self.title = title
+        self.text = text
+        self.creation = datetime.now()
+        self.update = self.creation
+
+
+
+    def __eq__(self, other):
+        if not isinstance(other, Post):
+            return False
+        return (self.code == other.code and
+                self.title == other.title and
+                self.text == other.text)
+
+    def __str__(self):
+        return f"Post(code={self.code}, title='{self.title}', text='{self.text[:20]}...')"
