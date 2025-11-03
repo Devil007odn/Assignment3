f51a7dc567944530c853c29cc723db4f6805f141
Yuvi <yuvraj21981@gmail.com>
2025-11-03 11:59:24 -0800

    Add unit tests for Blog class

---
+++ tests/blog_test.py
@@ -0,0 +1,45 @@
+import unittest
+from blogging.blog import Blog
+from blogging.post import Post
+
+class BlogTest(unittest.TestCase):
+
+    def setUp(self):
+        self.blog = Blog(1, "Travel", "travel.com", "travel@mail.com")
+
+    def test_create_post(self):
+        post = self.blog.create_post("Title", "Text")
+        self.assertEqual(post.title, "Title")
+        self.assertEqual(post.text, "Text")
+        self.assertEqual(post.code, 1)
+
+    def test_search_post(self):
+        self.blog.create_post("Title", "Text")
+        post = self.blog.search_post(1)
+        self.assertIsNotNone(post)
+        self.assertEqual(post.title, "Title")
+
+    def test_update_post(self):
+        self.blog.create_post("Title", "Text")
+        success = self.blog.update_post(1, "New Title", "New Text")
+        self.assertTrue(success)
+        post = self.blog.search_post(1)
+        self.assertEqual(post.title, "New Title")
+        self.assertEqual(post.text, "New Text")
+
+    def test_delete_post(self):
+        self.blog.create_post("Title", "Text")
+        success = self.blog.delete_post(1)
+        self.assertTrue(success)
+        self.assertIsNone(self.blog.search_post(1))
+
+    def test_list_posts(self):
+        self.blog.create_post("A", "Text A")
+        self.blog.create_post("B", "Text B")
+        posts = self.blog.list_posts()
+        self.assertEqual(len(posts), 2)
+        self.assertEqual(posts[0].title, "B")  # reversed order
+        self.assertEqual(posts[1].title, "A")
+
+if __name__ == '__main__':
+    unittest.main()
