abfc3413116c7a6f6a887920860b283713cc0900
Benjamin Nichols <ben@w206-012-252-167.wireless.uvic.ca>
2025-11-06 13:42:00 -0800

    Added comments for post.py

--- Blogging/post.py
+++ Blogging/post.py
@@ -6,19 +6,26 @@ class Post:
     """
 
     def __init__(self, code: int, title: str, text: str):
+        """ Initializes a new post with a unique code, title, and text """
         self.code = code
         self.title = title
         self.text = text
+        """ Automatically sets creation and update timestamps to current time """
         self.creation = datetime.now()
         self.update = self.creation
 
-    def update_timestamp(self):
-        self.update = datetime.now()
-
     def __repr__(self):
+        """ String representation of the post, useful for developers """
         return f"Post(code={self.code}, title='{self.title}')"
 
+    def update_timestamp(self):
+        """ Refresh the update timestamp to the current time 
+            Called whenever the post is modified """
+        self.update = datetime.now()
+
     def __eq__(self, other):
+        """ Compare the content of a post with another post 
+            Returns true if the code, title, and text are the same """
         if not isinstance(other, Post):
             return False
         return (self.code == other.code and
@@ -26,4 +33,6 @@ class Post:
                 self.text == other.text)
 
     def __str__(self):
+        """ String representation of the post, useful for users
+            Returns a concise 20-character summary of the post """
         return f"Post(code={self.code}, title='{self.title}', text='{self.text[:20]}...')"
